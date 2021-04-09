b6adf3691359b360cdbb76294dfc2ebfad87d69a
Igawa Hironobu <zin920314@gmail.com>
2021-04-09 10:06:46 +0900

    feat: Vueにvue-i18nプラグインをインストール

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import App from './App.vue'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
 
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages: {
+    ja: {},
+    en: {}
+  }
+})
+
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
