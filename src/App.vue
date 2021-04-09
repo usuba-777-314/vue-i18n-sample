1accc510f7879549dbed9a618de9f7302dfd4d34
Igawa Hironobu <zin920314@gmail.com>
2021-04-09 10:13:49 +0900

    feat: App.vueにmessageを設定

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
+<i18n>
+{
+  "en": {
+    "helloWorld": "Welcome to Your Vue.js App"
+  },
+  "ja": {
+    "helloWorld": "ようこそ、あなたのVue.jsアプリへ"
+  }
+}
+</i18n>
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="$t('helloWorld')"/>
   </div>
 </template>
 
